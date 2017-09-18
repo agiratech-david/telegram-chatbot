e4a3ffa723ce96d0878ca3da822fbf7f500696de
david <david2agiratech.com>
2017-09-18 15:25:00 +0530

    telegram bot

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ var bot = new TelegramBot('428231486:AAFMaaaB3fls5VczjVHkgPy5AIBAiwViKRs', {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(80);
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(8080, 'APP_PRIVATE_IP_ADDRESS');
+console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
